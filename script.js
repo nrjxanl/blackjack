ca0ab49b7f1a68fa45ea66b96459a19ad80aea46
nrjxanl <shwnn06@naver.com>
2023-11-03 14:55:06 +0900

    Co-authored-by: magicjoy <magicjoy@users.noreply.github.com> Co-authored-by: Bellbellbel <Bellbellbel@users.noreply.github.com>

--- script.js
+++ script.js
@@ -14,6 +14,10 @@
 // 카드 문양별 A, 2~10, J, K, Q 카드 생성(총 52개) - deck
 numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q"];
 deck = [];
+d_deck = [];
+p_deck = [];
+total = [0,0,0,0] //플레이어 토탈, 딜러 토탈, 플레이어 A 수, 딜러 A 수
+A_case = {"1":[1,10],"2":[2,11,20],"3":[3,12,21],"4":[4,13]}
 winning_count = 0;
 for (i = 0; i < 13; i++) {
     deck.push("h" + numbers[i]);
@@ -21,23 +25,20 @@ for (i = 0; i < 13; i++) {
     deck.push("s" + numbers[i]);
     deck.push("d" + numbers[i]);
 }
-console.log(deck)
+
+shuffle(deck)
 
 // array 내 함수 무작위로 섞는 함수 - function shuffle()
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 };
 
-// deck에서 몇 번째 카드를 뽑을지 지정할 array(0~51) 만들고 위 함수 사용하여 섞기 - iArray
-iArray = []
-for (i = 0; i < 52; i++) {
-    iArray.push(i);
-};
-shuffle(iArray);
 
 // 베팅 금액 초기값을 1,000원으로 지정 - inputValue
-inputValue = 1000;
-$("#input").text(inputValue);
+window.onload = function() {
+    inputValue = 1000;
+    $("#input").text(inputValue);
+}
 
 // up 버튼 누르면 베팅 금액 1,000원씩 올리고 inputValue에 그 값 저장하기
 function up() {
@@ -60,86 +61,101 @@ function down() {
         $("#down").css("pointer-events", "none")
     }
 }
-
+function sum_total(a,types) {
+    total[types] = 0
+    tem = ''
+    for(i=0;i<a.length;i++) {
+        tem = a[i].substring(1)
+        if(Number.isInteger(parseInt(tem))){
+            total[types] += parseInt(tem)
+        } else if(tem != "A") {
+            total[types] += 10
+        } else {
+            continue;
+        }
+    }
+    if (types == 1) {
+        let temp = [];
+        for(i = 0;i<A_case[[-1]].length;i++) {
+            temp.push((total(types) + A_case[[-1]][i]) <= 21? (total(types) + A_case[[-1]][i]) : 0)
+        }
+        total[types] = Math.max(temp)
+    }
+}
 // 시작 버튼 누를 시
 function start() {
 
     // 화면 좌측 상단에 베팅 금액 띄우기
     $("#betPrice").text(inputValue + "원");
+    $("#bet").css("display", "none");
+    $("#betPrice").css("display", "block");
+    $("#currentPoint").css("display", "block");
+    $("#hit").css("display", "block");
+    $("#stand").css("display", "block");
 
-    // 플레이어에게 deck의 iArray[0]번째, irray[1]번째 카드 지급(iArray[0]~iArray[25]: 플레이어 카드)
+    // 플레이어에게 deck의 카드 지급(iArray[0]~iArray[25]: 플레이어 카드)
     for (x = 0; x < 2; x++) {
-        $("#bet").css("display", "none");
-        $("#betPrice").css("display", "block");
-        $("#player").append("<div>" + deck[iArray[x]] + "</div>");
+        $("#player").append("<div>" + deck[0] + "</div>");
+        p_deck.push(deck[0]);
+        if (deck[0] == 'A') {
+            total[2] += 1
+        }
+        deck.shift();
     }
-
-    // 딜러에게 deck의 iArray[26]번째, iArray[27]번째 카드 지급(iArray[26]~iArray[51]: 딜러 카드)
-    for (y = 26; y < 28; y++) {
+    sum_total(p_deck,0)
+    $("#currentPoint > p:nth-of-type(1)").empty()
+    $("#currentPoint > p:nth-of-type(1)").append(total[0])
+    $("#currentPoint > p:nth-of-type(2)").empty()
+    $("#currentPoint > p:nth-of-type(2)").append(total[2])
+    // 딜러에게 deck의 카드 지급
+    for (y = 0; y < 2; y++) {
         $("#bet").css("display", "none");
-        $("#dealer").append("<div>" + deck[iArray[y]] + "</div>");
+        $("#dealer").append("<div>" + deck[0] + "</div>");
+        d_deck.push([0]);
+        if (deck[0] == 'A') {
+            total[3] += 1
+        }
+        deck.shift();
     }
 }
 
 // hit 버튼 누를 시
 function hit() {
 
-    // 위의 x, y 1씩 증가(x: 2~25, y: 28~51)
-    x++;
-    y++;
-
-    // 플레이어와 딜러에게 각각 deck의 iArray[x]번째, iArray[y]번째 카드 지급
-    $("#player").append("<div>" + deck[iArray[x]] + "</div>");
-    $("#dealer").append("<div>" + deck[iArray[y]] + "</div>");
+    // 플레이어에게 deck의 카드 지급
+    $("#player").append("<div>" + deck[0] + "</div>");
+    p_deck.push(deck[0])
+    if (deck[0] == 'A') {
+        total[2] += 1
+    }
+    deck.shift();
+    sum_total(p_deck,0)
+    $("#currentPoint > p:nth-of-type(1)").empty()
+    $("#currentPoint > p:nth-of-type(1)").append(total[0])
+    console.log(total[2])
+    $("#currentPoint > p:nth-of-type(2)").empty()
+    $("#currentPoint > p:nth-of-type(2)").append(total[2])
+    //21 넘을 시 게임 종료
 }
 
+function stand() {    
+    $("#selectPoint").css("display", "block");
+    $("#selectPoint").append(A_case[total[2]]);
+    p_deck = p_deck.filter((element) => element != 'hA'&&element != 'sA'&&element != 'dA' && element != 'cA')
+    p_deck.push('h'+10)
+    sum_total(p_deck,0)
+}
 
+function dealer_action() {
+    sum_total(d_deck,1)
+    while (total[1]<16) {
+        d_deck.push(deck[0]);
+        $("#dealer").append("<div>" + deck[0] + "</div>");
+        if (deck[0] == 'A') {
+            total[3] += 1
+        }
+        sum_total(d_deck,1)
+        deck.shift()
 
-
-
-
-
-
-
-
-
-
-// 카드섞기 
-// function shuffleDeck() {
-//     for (let i = numbers.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-//     }
-// }
-// shuffleDeck(deck);
-
-
-
-//initializeDeck (중복되지 않는 카드 뭉치를 생성)
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-//사용자 인터페이스 (hit stand)
-
-//const dealButton = document.getElementById("deal-button");
-//const hitButton = document.getElementById("hit-button");
-//const standButton = document.getElementById("stand-button");
-//const dealerHandElement = document.getElementById("dealer-hand");
-//const playerHandElement = document.getElementById("player-hand");
-//button 클릭시 이벤트 발생 만들어야함
+    }
+}
